0a1815d2a1e26ab6167a470903d43fa81ef90212
Елена Чувашева <chuvashevalena@yandex.ru>
2023-03-07 15:43:42 +0300

    connect with db

--- app/main.py
+++ app/main.py
@@ -1,13 +1,36 @@
 import uvicorn
 from fastapi import FastAPI
 from sqladmin import Admin, ModelView
+from sqlalchemy import select
 
-from app.models.posts import Test, engine
+from app.models.posts import Test, database, engine
 from app.routers import posts
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
 app.include_router(posts.router)
+
+@app.get("/")
+async def read_root():
+#    query = (
+#        select(
+#            [
+#                Test.id,
+#                Test.name,
+#            ]
+#        )
+#        .select_from(Test)
+#    )
+    query = select(Test)
+    return await database.fetch_all(query)
 
 admin = Admin(app, engine)
 
--- app/models/posts.py
+++ app/models/posts.py
@@ -1,10 +1,14 @@
+import databases
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-engine = create_async_engine('sqlite+aiosqlite:///./sql.db', echo=True)
+DATABASE_URL = 'sqlite+aiosqlite:///./sql.db'
+
+engine = create_async_engine(DATABASE_URL, future=True, echo=True)
+database = databases.Database(DATABASE_URL)
 
 class Test(Base):
     __tablename__ = 'test'
